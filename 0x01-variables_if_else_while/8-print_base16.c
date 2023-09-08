f5061f9d89111a01979de10e7b1951f97c55d630
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-08 16:18:53 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,15 @@
 int main(void)
 {
 	int b;
+	char c;
 
-	for (b = 0 ; b < 16 ; b++)
+	for (b = 0 ; b < 10 ; b++)
 	{
 		putchar('0' + b);
+	}
+	for (c = 'a' ; c <= 'f' ; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);

d5ae6d6e87f5b2421559834607cb362e7f5c22c8
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-08 13:26:43 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: A program to print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1 ; a < 10 ; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}

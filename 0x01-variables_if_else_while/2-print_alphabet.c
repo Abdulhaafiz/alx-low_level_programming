dff729b2b57e4daff25214922ff39e28621c0726
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-08 12:56:30 -0700

    last

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program to print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	for(a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}

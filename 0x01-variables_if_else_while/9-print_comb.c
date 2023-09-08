8872c353214c2ee3b26c1b8c9bb6002c4c1f16a1
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-08 16:34:15 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program to print
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0 ; d < 10 ; d++)
+	{
+		putchar(d);
+		putchar(,);
+		putchar( );
+	}
+	putchar('\n');
+	return (0);
+}

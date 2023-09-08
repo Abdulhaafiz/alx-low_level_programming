fe4376532e828047246801edb6567a8dc49f539b
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-08 14:05:35 -0700

    print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program to print digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

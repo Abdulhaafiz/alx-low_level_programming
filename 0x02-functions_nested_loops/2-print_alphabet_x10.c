a5059ff80c4f671e6d813da2c1bed58bb44cff84
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-12 02:26:57 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "print.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: "print_alphabet_x10()"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print.h
@@ -0,0 +1,16 @@
+void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
+{
+	int g;
+	char a;
+	a = 'a';
+	for (g = 1 ; g <= 10 ; g++)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+		{
+			putchar(a);
+		}
+			putchar('\n');
+	}
+}

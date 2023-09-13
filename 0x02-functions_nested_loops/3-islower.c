ca71cbe233f3a19ccba81937522cc7b884c0b4e5
Abdulhaafiz <miqdadbello21@gmail.com>
2023-09-13 16:17:57 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,19 @@
 /**
  * _islower - A function that returns 1 if c is a lowercase
  *
- * c - the character to check
+ * c: the character to check
  *
  * Return: 1 if c is a lowercase else 0
  */
 
 int _islower(int c)
 {
-	if (_islower(c)
-			{
-			return (1);
-			{
-			else
-			{
-			return (0);
-			}
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
